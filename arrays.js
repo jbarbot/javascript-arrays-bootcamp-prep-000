7aea8fd9115a347607c8c2366878229040586362
Jacob Barbot <barbotjacob@gmail.com>
2018-06-26 18:59:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,8 +22,9 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destrivelyRemoveElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift;
+  return array;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
